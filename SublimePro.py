e02615aa30a0ca11cbc9cc5b2347b6c1386ed80a
Ertjaet <ertjaet@gmail.com>
2013-06-12 15:53:49 -0400

    Edited formatting to match python standards

--- SublimePro.py
+++ SublimePro.py
@@ -24,7 +24,7 @@ class OpenProCommand(sublime_plugin.WindowCommand):
         return [[os.path.basename(p), p] for p in paths]
 
     def project_paths(self):
-        result = communicate(["pro","list"])
+        result = communicate(["pro", "list"])
         self.projects = result.split("\n")
         return self.projects
 
@@ -36,7 +36,7 @@ class OpenProCommand(sublime_plugin.WindowCommand):
             self.open_project(selpath)
 
     def open_project(self, path):
-        sublime_command_line(['-n',path])
+        sublime_command_line(['-n', path])
 
 
 # hack to add folders to sidebar (stolen from wbond)
@@ -53,6 +53,7 @@ def sublime_command_line(args):
     args.insert(0, get_sublime_path())
     return subprocess.Popen(args)
 
+
 # Command line utilities
 def memoize(f):
     rets = {}
@@ -66,87 +67,98 @@ def memoize(f):
     wrap.__name__ = f.__name__
     return wrap
 
+
 def extract_path(cmd, delim=':'):
     path = popen(cmd, os.environ).communicate()[0]
     path = path.split('__SUBL__', 1)[1].strip('\r\n')
     print(path)
     return ':'.join(path.split(delim))
 
+
 def find_path(env):
-  # find PATH using shell --login
-  if 'SHELL' in env:
-    shell_path = env['SHELL']
-    shell = os.path.basename(shell_path)
+    # find PATH using shell --login
+    if 'SHELL' in env:
+        shell_path = env['SHELL']
+        shell = os.path.basename(shell_path)
 
     if shell in ('bash', 'zsh'):
-      return extract_path(
-        [shell_path, '--login', '-c', 'echo __SUBL__$PATH']
-      )
+        return extract_path(
+            [shell_path, '--login', '-c', 'echo __SUBL__$PATH']
+        )
     elif shell == 'fish':
-      return extract_path(
-        [shell_path, '--login', '-c', 'echo __SUBL__; for p in $PATH; echo $p; end'],
-        '\n'
-      )
+        return extract_path(
+            [shell_path, '--login', '-c', 'echo __SUBL__; for p in $PATH; echo $p; end'],
+            '\n'
+        )
 
-  # guess PATH if we haven't returned yet
-  split = env['PATH'].split(':')
-  p = env['PATH']
-  for path in (
-    '/usr/bin', '/usr/local/bin',
-    '/usr/local/php/bin', '/usr/local/php5/bin'
-        ):
-    if not path in split:
-      p += (':' + path)
+    # guess PATH if we haven't returned yet
+    split = env['PATH'].split(':')
+    p = env['PATH']
+    for path in (
+        '/usr/bin',
+        '/usr/local/bin',
+        '/usr/local/php/bin',
+        '/usr/local/php5/bin'
+    ):
+        if not path in split:
+            p += (':' + path)
 
-  return p
+    return p
+
 
 @memoize
 def create_environment():
-  if os.name == 'posix':
-    os.environ['PATH'] = find_path(os.environ)
+    if os.name == 'posix':
+        os.environ['PATH'] = find_path(os.environ)
 
-  return os.environ
+    return os.environ
+
 
 def which(cmd, env=None):
-  if env is None:
-    env = create_environment()
+    if env is None:
+        env = create_environment()
 
-  for path in env['PATH'].split(':'):
-    full = os.path.join(path, cmd)
-    if os.path.isfile(full) and os.access(full, os.X_OK):
-      return full
+    for path in env['PATH'].split(':'):
+        full = os.path.join(path, cmd)
+        if os.path.isfile(full) and os.access(full, os.X_OK):
+            return full
+
 
 # popen methods
 def communicate(cmd, stdin=None, timeout=None, **popen_args):
-  p = popen(cmd, **popen_args)
-  if isinstance(p, subprocess.Popen):
-    out = p.communicate(stdin)
-    return (out[0] or '') + (out[1] or '')
-  elif isinstance(p, str):
-    return p
-  else:
-    return ''
+    p = popen(cmd, **popen_args)
+    if isinstance(p, subprocess.Popen):
+        out = p.communicate(stdin)
+        return (out[0] or '') + (out[1] or '')
+    elif isinstance(p, str):
+        return p
+    else:
+        return ''
+
 
 def popen(cmd, env=None, return_error=False):
-  if isinstance(cmd, str):
-    cmd = cmd,
+    if isinstance(cmd, str):
+        cmd = cmd,
 
-  info = None
-  if os.name == 'nt':
-    info = subprocess.STARTUPINFO()
-    info.dwFlags |= subprocess.STARTF_USESHOWWINDOW
-    info.wShowWindow = subprocess.SW_HIDE
+    info = None
+    if os.name == 'nt':
+        info = subprocess.STARTUPINFO()
+        info.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+        info.wShowWindow = subprocess.SW_HIDE
 
-  if env is None:
-    env = create_environment()
+    if env is None:
+        env = create_environment()
 
-  try:
-    return subprocess.Popen(cmd, stdin=subprocess.PIPE,
-      stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-      startupinfo=info, env=env)
-  except OSError as err:
-    print('Error launching', repr(cmd))
-    print('Error was:', err.strerror)
+    try:
+        return subprocess.Popen(
+            cmd,
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+            startupinfo=info,
+            env=env)
+    except OSError as err:
+        print('Error launching', repr(cmd))
+        print('Error was:', err.strerror)
 
-    if return_error:
-      return err.strerror
+        if return_error:
+            return err.strerror
